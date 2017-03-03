887a6a6dccc662b74d273d164c6f4c1c6a6f96fa
Hunter Dellere <hdellere@gmail.com>
2017-03-03 09:16:41 -0600

    3/3

---
+++ ReadMe.txt
@@ -0,0 +1,2 @@
+Required twitter package can be found at:
+  https://github.com/sixohsix/twitter
--- WUS.py
+++ WUS.py
@@ -6,7 +6,7 @@ a Instagram photo from the given location and reply back to the user with
 the retrieved image.
 """
 
-import Tokens
+from Tokens import token, tokenSecret, key, keySecret
 import time
 from twitter import Twitter, OAuth
 import re
@@ -14,32 +14,40 @@ import re
 # Set up twitter authentification
 twitter = Twitter(
     retry=True,
-    auth=OAuth(Tokens.token, Tokens.tokenSecret, Tokens.key, Tokens.keySecret))
+    auth=OAuth(token, tokenSecret, key, keySecret))
 
 #  Main Loop
 tweetInOld = 0
+calloutCheck = 0
+
 while(1):
-    time.sleep(75)  # API Timer delay
-    tweetIn = twitter.statuses.home_timeline()  # Pulls current timeline
+    time.sleep(3)  # API Timer delay
+    tweetIn = twitter.search.tweets(q='@ksu_bell')
+    # print(tweetIn)  # Uncomment to check current read tweet JSON data
+    # tweetIn = twitter.statuses.home_timeline()  # Pulls current timeline
     """Not quite sure how to make these two if statements into one, need the
     first on right now to take care of the first iteration when the
     tweetInOld is not valid for JSON extraction"""
     if tweetInOld == 0:  # First iteration
         tweetInOld = tweetIn
-        userName = tweetIn[0]['user']['name']  # Most recent tweet on timeline
-        tweetText = tweetIn[0]['text']
+        userName = tweetIn['statuses'][0]['user']['name']  # Most recent tweet
+        tweetText = tweetIn['statuses'][0]['text']
         normalText = str(tweetText)  # Makes JSON a string
-        print(normalText.lower())  # Test
-        print(userName, tweetText)  # Test
+        # print(normalText.lower())  # Uncomment for testing
+        # print(userName, tweetText)  # Uncomment for testing
         calloutCheck = re.search(r'@ksu_bell', normalText.lower())
     # Checks to see if a new tweet it present
-    elif (tweetIn[0]['user']['screen_name'] !=
-          tweetInOld[0]['user']['screen_name']):
+    elif (tweetIn['statuses'][0]['user']['name'] !=
+          tweetInOld['statuses'][0]['user']['name']):
         tweetInOld = tweetIn
-        userName = tweetIn[0]['user']['name']
-        tweetText = tweetIn[0]['text']
+        userName = tweetIn['statuses'][0]['user']['name']  # Most recent tweet
+        tweetText = tweetIn['statuses'][0]['text']
         normalText = str(tweetText)
         calloutCheck = re.search(r'@ksu_bell', normalText.lower())
-    if calloutCheck:
-        handle = calloutCheck.group()  # If executed, handle is present
-        print(handle)
+    if calloutCheck != 0:
+        """This section of the code will search the tweet for any format
+        of a location input which will be used to get lat and long from
+        the Google API"""
+        handle = calloutCheck.group()  # If executed, handle is present (test)
+        print(handle)  # Test
+        calloutCheck = 0  # Reset the calloutCheck variable to prevent loops
